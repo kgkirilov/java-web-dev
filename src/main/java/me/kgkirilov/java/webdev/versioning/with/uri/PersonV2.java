9ab892ae4ed7c7f128c3a4f750c2c06656b971f4
Krasimir Kirilov <k.kirilov92@gmail.com>
2019-05-28 16:36:18 +0300

    URI versioning

---
+++ src/main/java/me/kgkirilov/java/webdev/versioning/with/uri/PersonV2.java
@@ -0,0 +1,17 @@
+package me.kgkirilov.java.webdev.versioning.with.uri;
+
+public class PersonV2 {
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+}
